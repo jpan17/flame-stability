caac58120effe464f3ded7e684068b7ba66960f8
Jessica Pan <jzpan17@gmail.com>
2019-07-11 14:06:02 -0500

    also plotted cumulative changes

--- multiplePCA.py
+++ multiplePCA.py
@@ -7,11 +7,18 @@ def applyPCA (array, componentCount):
     
     principalComponents = pca.fit_transform(array)
     
+    
+    plt.subplot(121)
     # plot individual pcas
     for i in range(0, len(pca.explained_variance_ratio_)):
         plt.scatter(i, pca.explained_variance_ratio_[i],
                     c = 'orange')
         
-        
+    
+    cumulative = 0
+    plt.subplot(122)
+    for i in range(0, len(pca.explained_variance_ratio)):
+        cumulative += pca.explained_variance_ratio_[i]
+        plt.scatter(i, cumulative, c = 'red')
     
     plt.show()
