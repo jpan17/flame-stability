92d5d1bcabfea32600d1bc7b466806afef746bc8
Jessica Pan <jzpan17@gmail.com>
2019-07-12 11:35:14 -0500

    okay this is how it's gonna look for now

--- graph3d.py
+++ graph3d.py
@@ -3,43 +3,41 @@ import plotly
 plotly.tools.set_credentials_file(username='jp41', api_key='yXWv8yaCZstSiMBDB9Fu')
 import plotly.plotly as py
 
+
 def graph(array, title):
     
-    x = array[:,0]
-    y = array[:,1]
-    z = array[:,2]
     
-
-    data = []
-    clusters = []
-    colors = ['yellow', 'orange', 'red']
     
-    scatter = dict (
-        mode = 'markers',
-        name = 'y',
-        type = 'scatter3d',
-        x = x, y = y, z = z,
-        marker = dict(size = 2, color = "red")
-    )
+    # x = array[:,0]
+    # y = array[:,1]
+    # z = array[:,2]
+        
+    # scatter = dict (
+    #     mode = 'markers',
+    #     name = 'y',
+    #     type = 'scatter3d',
+    #     x = x, y = y, z = z,
+    #     marker = dict(size = 2, color = "red")
+    # )
     
-    clusters = dict (
-        alphahull = 7,
-        name = 'y',
-        opacity = 0.1,
-        type = 'mesh3d',
-        x = x, y = y, z = z
-    )
+    # clusters = dict (
+    #     alphahull = 7,
+    #     name = 'y',
+    #     opacity = 0.1,
+    #     type = 'mesh3d',
+    #     x = x, y = y, z = z
+    # )
     
-    layout = dict (
-        title = title,
-        scene = dict (
-            xaxis = dict (zeroline = False),
-            yaxis = dict (zeroline = False),
-            zaxis = dict (zeroline = False)
-        )
-    )
+    # layout = dict (
+    #     title = title,
+    #     scene = dict (
+    #         xaxis = dict (zeroline = False),
+    #         yaxis = dict (zeroline = False),
+    #         zaxis = dict (zeroline = False)
+    #     )
+    # )
     
-    fig = dict (data = [scatter, clusters], layout = layout)
-    py.plot(fig, filename = title)
+    # fig = dict (data = [scatter, clusters], layout = layout)
+    # py.plot(fig, filename = title)
     
     return
