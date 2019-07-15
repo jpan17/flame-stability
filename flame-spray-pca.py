031db353fa201e7ad4215f168a2f805afa98b2d5
Jessica Pan <jzpan17@gmail.com>
2019-07-15 15:04:49 -0500

    see if you can get all videos from csv

--- flame-spray-pca.py
+++ flame-spray-pca.py
@@ -7,42 +7,42 @@ import flameTest.luminance as luminance
 # main function
 def main():
     
+    df = pandas.read_csv('EtOH_flamemap.csv')
     
-    
-    features = []
-    
-    # read in video
-    fire = cv2.VideoCapture('')
-    
-    # print error message if you can't read it in
-    if (fire.isOpened() == False):
-        print("Error opening video file or stream")
+    for i in range(0, len(df['File name'])):
         
-    # initialize video variables
-    ret, frame = fire.read()
-    height, width, channels = frame.shape
-    vidHeight = height
-    vidWidth = width 
-    frameCount = 0
-    test = ''
-    
-    # display the video until 'q' is pressed or until it terminates
-    while (fire.isOpened()):
-        ret, frame = fire.read()
-        frameCount += 1
+        # read in video
+        fire = cv2.VideoCapture('./fireFiles/' + df['File name'][i])
         
-        if ret == True:
-            cv2.imshow('Fire', frame)
-               
-            # terminates the video before it finishes
-            if cv2.waitKey(25) == ord('q') or frameCount > 2800:
-                break
+        # print error message if you can't read it in
+        if (fire.isOpened() == False):
+            print("Error opening video file or stream")
             
-        else:
-            break
-    
-    fire.release()
-    cv2.destroyAllWindows()
+        # initialize video variables
+        ret, frame = fire.read()
+        height, width, channels = frame.shape
+        vidHeight = height
+        vidWidth = width 
+        frameCount = 0
+        test = ''
+        
+        # display the video until 'q' is pressed or until it terminates
+        while (fire.isOpened()):
+            ret, frame = fire.read()
+            frameCount += 1
+            
+            if ret == True:
+                cv2.imshow('Fire', frame)
+                
+                # terminates the video before it finishes
+                if cv2.waitKey(25) == ord('q') or frameCount > 2800:
+                    break
+                
+            else:
+                break
+        
+        fire.release()
+        cv2.destroyAllWindows()
    
         
 if __name__ == "__main__":
