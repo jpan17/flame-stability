4fde50f1825b2da608cf3db3a66cb13e40b217d5
Jessica Pan <jzpan17@gmail.com>
2019-07-16 15:56:45 -0500

    edge detection works now; fix up parameters to make it look smoother

--- edge-detection.py
+++ edge-detection.py
@@ -1,38 +1,45 @@
 import cv2
 import numpy as np
+import pandas
 # =========================================================================== #
 
-df = pandas.read_csv('EtOH_flamemap.csv')
-
 def edgeDetection():
+    df = pandas.read_csv('EtOH_flamemap.csv')
+    
     for i in range(0, len(df['File name'])):
         
-        fire = cv2.VideoCapture('./fireFiles/' + df['File name'][i])
+        fire = cv2.VideoCapture('.\\fireFiles\\' + df['File name'][i])
         
         if (fire.isOpened() == False):
             print("Error opening video file or stream")
-    
-        ret, frame = fire.read()
         
         while (fire.isOpened()):
-                    
-            laplacian = cv2.Laplacean(frame, cv2.CV_64F)
-            sobelx = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize = 5)
-            sobely = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize = 5)
-            edges = cv2.canny(frame, 100, 200)
             
+            ret, frame = fire.read()
+    
+            if ret == True:
+                        
+                laplacian = cv2.Laplacian(frame, cv2.CV_64F)
+                sobelx = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize = 5)
+                sobely = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize = 5)
+                edges = cv2.Canny(frame, 25, 25)
+                
+                
+                cv2.imshow('default', frame)
+                cv2.imshow('laplacian', laplacian)
+                cv2.imshow('sobelx', sobelx)
+                cv2.imshow('sobely', sobely)
+                cv2.imshow('edges', edges)
+            else:
+                break
             
-            cv2.imshow('default', frame)
-            cv2.imshow('laplacian', laplacian)
-            cv2.imshow('sobelx', sobelx)
-            cv2.imshow('sobely', sobely)
-            cv2.imshow('edges', edges)
-            
-            if cv2.waitKey(5) & 0xFF:
-            break
+            if cv2.waitKey(25) == ord('q'):
+                print('hey')
+                break
         
-    cv2.destroyAllWindows()
+    
     fire.release()
+    cv2.destroyAllWindows()
     
 
 if __name__ == "__main__":
